d3de300751d90b13cac3426aab556ec902894c5f
mohamadp91 <mohamad7291m@gmail.com>
2021-09-17 13:18:25 +0430

    fetch weather api

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,20 @@
 import React, { useEffect, useState } from "react"
+import env from "@beam-australia/react-env"
+import axios from "axios"
 
 import { Sidebar } from "../sidebar"
 
 export const Home = () => {
 	const [regionName, setRegionName] = useState(null)
 
+	useEffect(() => {
+		regionName &&
+			axios
+				.get(`${env("API_HOST")}?key=${env("API_TOKEN")}&q=${regionName}`)
+				.then((r) => {
+					console.log(r)
+				})
+	}, [regionName])
 	return (
 		<>
 			<Sidebar setRegionName={setRegionName} />
